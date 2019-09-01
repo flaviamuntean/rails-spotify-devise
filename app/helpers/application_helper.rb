1dd97784bf8ab1d6e6702194f8a0c60ed8b011e0
Flavia Muntean <muntean.flavia@gmail.com>
2019-09-01 23:00:22 +0100

    a bit of styling for messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
-  def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
+  def fetch_recipient(conversation)
+    if conversation.sender_id == current_user.id || conversation.recipient_id == current_user.id
+      if conversation.sender_id == current_user.id
+        recipient = User.find(conversation.recipient_id)
+      else
+        recipient = User.find(conversation.sender_id)
+      end
+    end
+    return recipient
   end
 end
